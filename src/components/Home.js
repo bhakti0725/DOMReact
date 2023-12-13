25354f030da0a19f2ea3bde34397453fca194a1b
bhakti0725 <balansebhakti0703@gmail.com>
2023-12-13 20:23:31 +0530

    first-commit

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Header.css";
+const Home = () => {
+  return (
+    <header>
+      <h1>Welcome to React Router DOM Tutorials</h1>
+
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum magni
+        est et odit dolorem officia sed voluptatibus aperiam provident nam alias
+        tempore sint assumenda, consequatur qui error eveniet magnam
+        aliquid.pxex Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Harum magni est et odit dolorem officia sed voluptatibus aperiam
+        provident nam alias tempore sint assumenda, consequatur qui error
+        eveniet magnam aliquid.
+      </p>
+    </header>
+  );
+};
+
+export default Home;
---
+++ src/data/productData.js
@@ -0,0 +1,74 @@
+const products = [
+  {
+    id: 1,
+    name: "iPhone 12 Pro",
+    price: 999,
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum magni est et odit dolorem officia sed voluptatibus aperiam provident nam aliastempore sint assumenda, consequatur qui error eveniet magnam aliquid.",
+    image:
+      "https://cdn.pixabay.com/photo/2020/11/22/11/53/iphone-12-5766344_1280.jpg",
+    brand: "Apple",
+    category: "Phones",
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    id: 2,
+    name: "iPhone 12 Pro Max",
+    price: 1099,
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum magni est et odit dolorem officia sed voluptatibus aperiam provident nam aliastempore sint assumenda, consequatur qui error eveniet magnam aliquid.",
+    image:
+      "https://cdn.pixabay.com/photo/2020/11/18/13/51/iphone-12-5755365_1280.jpg",
+    brand: "Apple",
+    category: "Phones",
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    id: 3,
+    name: "Laptop",
+    price: 599,
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum magni est et odit dolorem officia sed voluptatibus aperiam provident nam aliastempore sint assumenda, consequatur qui error eveniet magnam aliquid.",
+    image:
+      "https://cdn.pixabay.com/photo/2017/10/10/21/46/laptop-2838914_1280.jpg",
+    brand: "Apple",
+    category: "Phones",
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    id: 4,
+    name: "Macbook Pro",
+    price: 1299,
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum magni est et odit dolorem officia sed voluptatibus aperiam provident nam aliastempore sint assumenda, consequatur qui error eveniet magnam aliquid.",
+    image:
+      "https://cdn.pixabay.com/photo/2015/11/19/11/45/macbook-pro-1050973_1280.jpg",
+    brand: "Apple",
+    category: "Laptops",
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    id: 5,
+    name: "Macbook Air",
+    price: 999,
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum magni est et odit dolorem officia sed voluptatibus aperiam provident nam aliastempore sint assumenda, consequatur qui error eveniet magnam aliquid.",
+    image:
+      "https://cdn.pixabay.com/photo/2017/06/07/21/29/macbook-pro-2381729_1280.jpg",
+    brand: "Apple",
+    category: "Laptops",
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+];
+
+export default products;
